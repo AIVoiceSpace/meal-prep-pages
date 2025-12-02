7890d31a5059b2751cbea1e864f8283d455add7f
brian-aivoice <brian-aivoice@users.noreply.github.com>
2025-12-02 19:31:37 +0000

    deploy: d9711510b1a8b75efabaef2fe109f41a1196e591

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,4 +1,40 @@
 var store = [{
+        "title": "Convenience Meal Prep: Your Ultimate Guide",
+        "excerpt":"Introduction We’ve all been there - after a long day of work, the last thing you want to do is spend hours in the kitchen cooking dinner. This is where meal prep comes into play. By preparing meals in advance, you can spend less time cooking and more time enjoying...","categories": ["Guides"],
+        "tags": ["convenience","easy","guide","meal-prep","quick"],
+        "url": "/2025/12/03/convenience-guide/",
+        "teaser": null
+      },{
+        "title": "Mastering Meal Prep for Different Diets: A Comprehensive Guide",
+        "excerpt":"Introduction Embarking on a new diet can be challenging. But what’s even more challenging is planning, preparing, and storing meals that align with your dietary needs. Whether you’re on a ketogenic (keto) diet, a palaeolithic (paleo) diet, or any other diet, meal prep is a game-changer. In this guide, you’ll...","categories": ["Guides"],
+        "tags": ["diets","guide","keto","meal-prep","paleo"],
+        "url": "/2025/12/03/diets-guide/",
+        "teaser": null
+      },{
+        "title": "Meal Prep Basics: Your Guide to Mastering the Art of Meal Preparation",
+        "excerpt":"Introduction Once upon a time, we all faced the dilemma of what to eat after a long, tiring day. Cooking seems like a chore, and ordering takeout isn’t always the healthiest (or cheapest) option. That’s where meal prep comes in. Not only does it save you time and money, but...","categories": ["Guides"],
+        "tags": ["food-prep","meal-planning","meal-prep"],
+        "url": "/2025/12/03/meal-prep-basics-guide/",
+        "teaser": null
+      },{
+        "title": "Sauces & Seasonings: The Secret to Flavorful Meal Prep",
+        "excerpt":"Introduction We’ve all been there: you’ve meal prepped for the week, but by Wednesday, you’re tired of the same old chicken and veggies. The solution? Sauces and seasonings. They can transform your staple foods into a variety of delicious meals, keeping your taste buds excited and your meal prep game...","categories": ["Guides"],
+        "tags": ["dressing","marinade","meal-prep","sauce","sauces"],
+        "url": "/2025/12/03/sauces-seasonings-guide/",
+        "teaser": null
+      },{
+        "title": "The Ultimate Storage & Containers Guide for Meal Preppers",
+        "excerpt":"Introduction: Why Good Storage Matters The right storage container can transform your meal prep experience. It’s not just about keeping food fresh, but also about organizing your fridge, transporting meals, and even serving sizes. This guide explores what makes a good meal prep container and dives into the best options...","categories": ["Gear"],
+        "tags": ["container","containers","mason-jar","meal-prep","storage"],
+        "url": "/2025/12/03/storage-containers-guide/",
+        "teaser": null
+      },{
+        "title": "Mastering the Art of Prepping Veggies and Sides",
+        "excerpt":"Introduction We’ve all faced the conundrum: you’ve batch-cooked a delicious protein, but what about the sides? How can you efficiently prep vegetables and grains that stay fresh, taste great, and complement your main dish? This guide is here to help. We’ll share expert tips and proven techniques from the r/MealPrepSunday...","categories": ["Guides"],
+        "tags": ["grain","meal-prep","pasta","rice","sides"],
+        "url": "/2025/12/03/vegetables-sides-guide/",
+        "teaser": null
+      },{
         "title": "Keto Meal Prep for a Low-Carb Lifestyle: Fat-Burning, Flavor-Packed Recipes",
         "excerpt":"Recipe Overview Embarking on a ketogenic lifestyle doesn’t mean compromising on flavor or variety. These low-carb, high-fat recipes are designed to keep you in ketosis, while providing ample nutrition and taste. The beauty of this type of meal prep is that it’s particularly cost-effective, as many keto ingredients like oils,...","categories": ["Recipes"],
         "tags": ["keto","low-carb","high-fat","macros","fat-burning"],
